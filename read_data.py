70690b514f7adcc7bbd5b41168720e5d8a3e9665
Leonard <thongyixuen@gmail.com>
2021-04-05 16:35:06 -0400

    Update gitignore

--- read_data.py
+++ read_data.py
@@ -1,4 +1,4 @@
 from import_libraries import pd
 
-path = "/Users/thongyixuen/Projects/research/knowledge-graph/"
+path = "/Users/leonardthong/Projects/research/knowledge-graph/"
 candidate_sentences = pd.read_csv(path + "wiki_sentences_v2.csv")
